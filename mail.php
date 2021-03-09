46c83ca162ae634018bef2450a693c96f5d97931
Roody Exantus <roodyprog@gmail.com>
2021-03-09 16:48:01 +0100

    modif section portfolio

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "5656";
+// Le message
+$message = "Line 1\r\nLine 2\r\nLine 3";
+
+// Dans le cas où nos lignes comportent plus de 70 caractères, nous les coupons en utilisant wordwrap()
+$message = wordwrap($message, 70, "\r\n");
+
+// Envoi du mail
+mail('roodyprog@gmail.com', 'Mon Sujet', $message);
